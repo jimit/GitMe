02dc0792e4f57324b32c5b924c92a00e6abcaca0
JIMIT <patel.jimit@gmail.com>
2012-03-09 16:26:13 -0500

    Initial Commit

---
+++ GitMe/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GitMe
+//
+//  Created by Jimit Patel on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GitMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitMe
+//
+//  Created by Jimit Patel on 3/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
